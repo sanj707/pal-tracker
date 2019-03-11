a4fdaf2a5a3f9c63fe68534778415ca24473ec77
joe <sanjoe.john@wipro.com>
2019-03-10 23:42:24 -0700

    First commit

---
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalTrackerApplication {
+
+    public static void main(String arg[]){
+        SpringApplication.run(PalTrackerApplication.class,arg);
+    }
+}
